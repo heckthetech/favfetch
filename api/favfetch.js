70415e249eb0380e3df6775f3f855c218934cb1f
heckthetech <samiulsamir032@gmail.com>
2025-06-26 21:56:46 +0600

    Update favfetch.js

--- api/favfetch.js
+++ api/favfetch.js
@@ -6,6 +6,8 @@ export const config = {
 const customFavicons = {
   'web.whatsapp.com': 'https://heckthetech.github.io/favfetch/api/whatsapp.png',
   'messenger.com': 'https://heckthetech.github.io/favfetch/api/messenger.png',
+  'youtube.com/watch?v=dQw4w9WgXcQ': 'https://heckthetech.github.io/favfetch/api/rick.gif',
+  'rickastley.co.uk': 'https://heckthetech.github.io/favfetch/api/rick.gif',
   // Add more here
 };
 
