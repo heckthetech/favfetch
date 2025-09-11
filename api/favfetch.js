1094f0ece713a643b2922ddaa28e4918d9c17d33
heckthetech <217954684+heckthetech@users.noreply.github.com>
2025-09-12 00:22:06 +0600

    Update favfetch.js

--- api/favfetch.js
+++ api/favfetch.js
@@ -14,7 +14,8 @@ const customFavicons = {
   'drive.google.com': 'https://heckthetech.github.io/favfetch/api/gdrive.png',
   'docs.google.com': 'https://heckthetech.github.io/favfetch/api/gdocs.webp',
   'docs.google.com': 'https://heckthetech.github.io/favfetch/api/gdocs.webp',
-  'heckthetech.github.io/ecplay': 'https://heckthetech.github.io/favfetch/api/chronaplay.png'
+  'heckthetech.github.io/ecplay': 'https://heckthetech.github.io/favfetch/api/chronaplay.png',
+  'excel.cloud.microsoft': 'https://heckthetech.github.io/favfetch/api/msexcel.jpg'
   // Add more here
 };
 
