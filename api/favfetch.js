8be9e6836119a0eb8ef8933501d96d49cffd9f30
heckthetech <217954684+heckthetech@users.noreply.github.com>
2026-01-26 00:57:44 +0600

    Update favfetch.js

--- api/favfetch.js
+++ api/favfetch.js
@@ -46,7 +46,8 @@ const customFavicons = {
   'heckthetech.github.io/ecplay': 'https://heckthetech.github.io/favfetch/api/chronaplay.webp',
   'photos.google.com': 'https://heckthetech.github.io/favfetch/api/photos.webp',
   'epicgames.com': 'https://heckthetech.github.io/favfetch/api/epic.webp',
-  'medico.com.bd': 'https://heckthetech.github.io/favfetch/api/userrequested/medico.webp'
+  'medico.com.bd': 'https://heckthetech.github.io/favfetch/api/userrequested/medico.webp',
+  'pinterest.com': 'https://heckthetech.github.io/favfetch/api/userrequested/Pinterest.webp'
 };
 
 
